b28fd93e6012397067dcdd377f8fea75a2ae4f8f
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-06-03 02:06:08 +0100

    Remove unused imports in AvailabilityToggle.js

--- client/src/components/AvailabilityToggle.js
+++ client/src/components/AvailabilityToggle.js
@@ -1,6 +1,5 @@
 // src/components/AvailabilityToggle.js
 import React from 'react';
-import { View, Text, Switch, StyleSheet } from 'react-native';
 
 export default function AvailabilityToggle({ available, setAvailable }) {
   return (
