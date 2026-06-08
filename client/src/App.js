8046d913aafbf45733dd140f8074f2993272baea
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-06-08 17:23:39 +0100

    Refactor App component to use React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-// App.js
+// src/App.js
 import React from 'react';
-import AppNavigator from './navigation/AppNavigator';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const App = () => {
+// Import your screens
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import SearchScreen from './screens/SearchScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
+
+
+export default function App() {
   return (
-    <AppNavigator />
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
+        <Route path="/home" element={<HomeScreen />} />
+        <Route path="/profile" element={<ProfileScreen />} />
+        <Route path="/search" element={<SearchScreen />} />
+         <Route path="/Favorites" element={<FavoritesScreen />} />
+      </Routes>
+    </Router>
   );
-};
-
-export default App;
+}
